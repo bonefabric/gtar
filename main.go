eb812974f1c461793e35fa442da9e6f59d4159c5
bonefabric <iburanguloff@ya.ru>
2023-02-05 14:17:52 +0300

    fixes & refactoring

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"archive/tar"
+	"compress/gzip"
 	"flag"
 	"fmt"
 	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 var xflag bool
@@ -15,33 +17,146 @@ var fflag string
 
 func init() {
 	flag.BoolVar(&xflag, "x", false, "extract from archive")
-	flag.StringVar(&fflag, "f", "out.tar", "archive file name")
+	flag.StringVar(&fflag, "f", "out.tar.gz", "archive file name")
 
 	flag.Parse()
 }
 
 func main() {
+	var op func() error
 	if xflag {
-		untar()
+		op = untar
 	} else {
-		totar()
+		op = totar
+	}
+
+	if err := op(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
 
-func untar() {
-	if len(flag.Args()) != 1 {
-		fmt.Println("invalid args - required extracting path")
+func totar() (err error) {
+	if len(flag.Args()) == 0 {
+		fmt.Println("missing files to create archive")
 		os.Exit(1)
 	}
 
+	abstar, err := filepath.Abs(fflag)
+	if err != nil {
+		return fmt.Errorf("failed to find absolute path to archive file: %s", err)
+	}
+
+	tarfile, err := os.Create(abstar)
+	if err != nil {
+		return fmt.Errorf("failed to create archive file: %s", err)
+	}
+
+	defer func(tarfile *os.File) {
+		if e := tarfile.Close(); e != nil && err == nil {
+			err = fmt.Errorf("failed to close archive file: %s", e)
+		}
+	}(tarfile)
+
+	tw := tar.NewWriter(tarfile)
+	defer func(tw *tar.Writer) {
+		if e := tw.Close(); e != nil && err == nil {
+			err = fmt.Errorf("failed to close archive writer: %s", e)
+		}
+	}(tw)
+
+	if strings.HasSuffix(filepath.Base(abstar), ".gzip") || strings.HasSuffix(filepath.Base(abstar), ".gz") {
+		zw := gzip.NewWriter(tarfile)
+		defer func(zw *gzip.Writer) {
+			if e := zw.Close(); e != nil && err == nil {
+				err = fmt.Errorf("failed to close gzip writer: %s", e)
+			}
+		}(zw)
+
+		tw = tar.NewWriter(zw)
+	}
+
+	for _, arg := range flag.Args() {
+		basepath := filepath.Clean(arg)
+
+		walker := func(path string, d fs.DirEntry, er error) (e error) {
+			if er != nil {
+				return er
+			}
+
+			abspath, e := filepath.Abs(path)
+			if e != nil || abspath == abstar {
+				return
+			}
+
+			finfo, e := d.Info()
+			if e != nil {
+				return
+			}
+
+			hdr, e := tar.FileInfoHeader(finfo, d.Name())
+			if e != nil {
+				return
+			}
+
+			relpath := path
+			if filepath.IsAbs(path) {
+				relpath, e = filepath.Rel(basepath, path)
+				if e != nil {
+					return
+				}
+			}
+			hdr.Name = relpath
+
+			if e = tw.WriteHeader(hdr); e != nil {
+				return
+			}
+
+			if d.IsDir() {
+				return
+			}
+
+			src, e := os.Open(path)
+			if e != nil {
+				return
+			}
+
+			defer func(src *os.File) {
+				if err := src.Close(); err != nil && e == nil {
+					e = err
+				}
+			}(src)
+
+			_, e = io.Copy(tw, src)
+			return
+		}
+
+		if err = filepath.WalkDir(basepath, walker); err != nil {
+			return err
+		}
+	}
+	return
+}
+
+func untar() (err error) {
+	if len(flag.Args()) != 1 {
+		return fmt.Errorf("invalid args - required extracting path")
+	}
+
 	abstar, err := filepath.Abs(filepath.Clean(fflag))
-	mustNotErr(err, fmt.Sprintf("failed to find tar %s: %s", fflag, err))
+	if err != nil {
+		return fmt.Errorf("failed to find archive absolute path: %s", err)
+	}
 
 	expath, err := filepath.Abs(filepath.Clean(flag.Args()[0]))
-	mustNotErr(err, fmt.Sprintf("failed to find extracting path %s: %s", fflag, err))
+	if err != nil {
+		return fmt.Errorf("failed to find extract absolute path: %s", err)
+	}
 
 	tarfile, err := os.Open(abstar)
-	mustNotErr(err, fmt.Sprintf("failed to open archive: %s", err))
+	if err != nil {
+		return fmt.Errorf("failed to open archive file: %s", err)
+	}
 
 	defer func(tarfile *os.File) {
 		if err := tarfile.Close(); err != nil {
@@ -51,130 +166,53 @@ func untar() {
 
 	tr := tar.NewReader(tarfile)
 
-	for {
-		hdr, err := tr.Next()
-		if err != nil {
-			if err == io.EOF {
-				break
+	if strings.HasSuffix(filepath.Base(abstar), ".gzip") || strings.HasSuffix(filepath.Base(abstar), ".gz") {
+		zr, zerr := gzip.NewReader(tarfile)
+		if zerr != nil {
+			return fmt.Errorf("failed to open gzip reader: %s", zerr)
+		}
+
+		defer func(zr *gzip.Reader) {
+			if e := zr.Close(); e != nil && err == nil {
+				err = fmt.Errorf("failed to close gzip reader: %s", e)
 			}
-			fmt.Printf("failed to extract file from archive: %s\n", err)
-			continue
+		}(zr)
+		tr = tar.NewReader(zr)
+	}
+
+	for {
+		hdr, e := tr.Next()
+		if e == io.EOF {
+			break
+		}
+		if e != nil {
+			err = fmt.Errorf("failed to extract archive: %s", e)
+			return
 		}
 
 		abspath := filepath.Join(expath, hdr.Name)
 
 		if hdr.FileInfo().IsDir() {
-			if err := os.MkdirAll(abspath, 0775); err != nil {
-				fmt.Printf("failed to extract create directiory %s: %s\n", abspath, err)
+			if err = os.MkdirAll(abspath, 0775); err != nil {
+				return fmt.Errorf("failed to make directories: %s", err)
 			}
 			continue
 		}
 
-		file, err := os.Create(abspath)
+		file, err := os.OpenFile(abspath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, hdr.FileInfo().Mode().Perm())
 		if err != nil {
-			fmt.Printf("failed to create file %s: %s; skipping\n", abspath, err)
-			continue
+			return fmt.Errorf("failed to create file %s: %s", abspath, err)
 		}
 
 		_, cperr := io.Copy(file, tr)
 
-		if clserr := file.Close(); err != nil {
-			fmt.Printf("failed to close file %s: %s\n", abspath, clserr)
-			continue
+		if clserr := file.Close(); clserr != nil {
+			return fmt.Errorf("failed to close file %s: %s", abspath, clserr)
 		}
 
 		if cperr != nil {
-			fmt.Printf("failed to copy data to file %s: %s\n", abspath, err)
-			continue
+			return fmt.Errorf("failed to copy data to %s: %s", abspath, cperr)
 		}
 	}
-}
-
-func totar() {
-	if len(flag.Args()) == 0 {
-		fmt.Println("missing files to create archive")
-		os.Exit(1)
-	}
-
-	abstar, err := filepath.Abs(fflag)
-	mustNotErr(err, fmt.Sprintf("failed to create tar file: %s", err))
-
-	tarfile, err := os.Create(abstar)
-	mustNotErr(err, fmt.Sprintf("failed to create tar file: %s", err))
-
-	defer func(tarfile *os.File) {
-		if err := tarfile.Close(); err != nil {
-			fmt.Printf("failed to close tar file: %s\n", err)
-		}
-	}(tarfile)
-
-	tw := tar.NewWriter(tarfile)
-	defer func(tw *tar.Writer) {
-		if err := tw.Close(); err != nil {
-			fmt.Printf("failed to close tar writer: %s\n", err)
-		}
-	}(tw)
-
-	for _, arg := range flag.Args() {
-		basepath, err := filepath.Abs(arg)
-		if err != nil {
-			fmt.Printf("failed to find absolute path to argument %s: %s; skipping\n", arg, err)
-			continue
-		}
-
-		walker := func(path string, d fs.DirEntry, err error) (e error) {
-			if err != nil || path == abstar {
-				return err
-			}
-
-			finfo, err := d.Info()
-			if err != nil {
-				return err
-			}
-
-			hdr, err := tar.FileInfoHeader(finfo, d.Name())
-			if err != nil {
-				return err
-			}
-
-			hdr.Name, e = filepath.Rel(basepath, path)
-			if e != nil {
-				return
-			}
-
-			if e = tw.WriteHeader(hdr); e != nil {
-				return
-			}
-
-			if d.IsDir() {
-				return nil
-			}
-
-			src, e := os.Open(path)
-			if e != nil {
-				return
-			}
-
-			defer func(src *os.File) {
-				if err := src.Close(); err != nil {
-					e = err
-				}
-			}(src)
-
-			_, e = io.Copy(tw, src)
-			return
-		}
-
-		if err := filepath.WalkDir(basepath, walker); err != nil {
-			fmt.Printf("failed to archive %s: %s; skipping\n", arg, err)
-			continue
-		}
-	}
-}
-
-func mustNotErr(err error, mess string) {
-	if err != nil {
-		fmt.Println(mess)
-		os.Exit(1)
-	}
+	return
 }
